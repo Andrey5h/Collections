d75936159840217fc97e4e41b207ba67cdd10b8c
Andrey5h <maxhaste@yandex.ru>
2017-12-25 20:28:21 +0300

    Second Commit

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -17,20 +17,14 @@ public class Base {
         }
         //Выводим все эелементы из файла
         System.out.println("В файле есть такие слова: " + list);
-
-
         //сортируем элементы по алфавиту и делаем подсчет слов из файле
         Set<String> sortedSet = new TreeSet<>();
         Map<String, Integer> statistics = new HashMap<>();
-
-
-        String max[];
-        max = new String[1];
-
         //Сначала преобразуем массив ArrayList в TreeSet для сортировки записей.
-
         int k = 0;
         String d;
+        String max[];
+        max = new String[1];
 
         for (int i = 0; i < list.size(); i++) {
             //добавляем записи из массива ArrayList в TreeSet
@@ -38,7 +32,6 @@ public class Base {
             sortedSet.add(d);
 
             // Сразу делаем подсчет слова из файла и записыаем их Мап.
-
             Integer count = statistics.get(d);
             if (count == null) {
                 count = 0;
@@ -47,14 +40,11 @@ public class Base {
             statistics.put(d, ++count);
 
             //Находим слово с максимальным повторением
-
             if (count > k) {
                 k = count;
                 max[0] = d;
-
             }
         }
-
         System.out.println("Сортируем по алфавиту: = " + sortedSet);
         System.out.println("Подсчет: = " + statistics);
         System.out.println("Наибольшее: = " + max[0]+ " Колличество повторений = " +k);
